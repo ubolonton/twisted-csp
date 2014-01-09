1abc72c616b700083a3a81cd5c7ec5a5550e8bc2
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-01-09 21:27:02 +0700

    update select example

--- example/select.py
+++ example/select.py
@@ -1,41 +1,20 @@
 import csp
 
 
-
-@csp.process
-def one(chan):
-    start, end = yield csp.wait(0.5)
-    print end - start, start, end
-    yield chan.put("one")
-
-
-@csp.process
-def two(chan):
-    start, end = yield csp.wait(0.5)
-    print end - start, start, end
-    yield chan.put("two")
-
-
-def q(chan):
-    start, end = yield csp.wait(0.5)
-    print end - start, start, end
-    yield chan.put(None)
+def produce(chan, value):
+    yield csp.wait(0.1)
+    yield csp.put(chan, value)
 
 
 def main():
-    chan1 = csp.Channel()
-    chan2 = csp.Channel()
-    quit = csp.Channel()
+    chans = []
+    for i in range(10):
+        chan = csp.Channel()
+        csp.go(produce(chan, i))
+        chans.append(chan)
 
-    one(chan1)
-    two(chan2)
-    csp.go(q(quit))
+    yield csp.wait(0.3)
 
     while True:
-        chan = yield csp.select(chan1, chan2, quit)
-        if chan is chan1:
-            print "1", (yield chan1.take())
-        if chan is chan2:
-            print "2", (yield chan2.take())
-        if chan is quit:
-            print "quit", (yield quit.take())
+        value, chan = yield csp.alts(chans)
+        print value

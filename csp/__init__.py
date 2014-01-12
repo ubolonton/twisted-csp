4ac79f9e16c7626b4b129bfe947cd6908b08a78d
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-01-12 11:34:35 +0700

    expose buffers from csp module, but not Process and FnHandler

--- csp/__init__.py
+++ csp/__init__.py
@@ -1,18 +1,18 @@
 # API
 from csp.impl.process import put, take, wait, alts, stop
 from csp.impl.channels import ManyToManyChannel as Channel
+from csp.impl.buffers import FixedBuffer, DroppingBuffer, SlidingBuffer
 
-from csp.impl.process import Process, FnHandler
-from csp.impl.buffers import FixedBuffer
+import csp.impl.process
 
 def go(gen):
     channel = Channel(FixedBuffer(1))
     def done(value):
         if value is not None:
-            channel.put(value, FnHandler(lambda: None))
+            channel.put(value, csp.impl.process.FnHandler(lambda: None))
         channel.close()
 
-    process = Process(gen, done)
+    process = csp.impl.process.Process(gen, done)
     process.run()
     return channel
 

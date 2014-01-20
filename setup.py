0f2af33907c20fa2796b70a4c9321065dbeb8607
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-01-20 21:55:00 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name="twisted-csp",
+      version="0.1.0",
+      description="Go-style channels for Twisted",
+      author="Nguyễn Tuấn Anh",
+      author_email="ubolonton@gmail.com",
+      url="https://github.com/ubolonton/twisted-csp",
+      long_description=open('README.md').read(),
+
+      install_requires = [
+          "twisted"
+      ]
+)

4a81ddd3ef5b5bcb37a1ab8dfacc961fff60816b
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-01-31 16:31:24 +0700

    ok, what the heck

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name="twisted-csp",
-      packages=["csp"],
-      version="0.1.3",
+      version="0.1.4",
       description="Go-style channels for Twisted",
       author="Nguyễn Tuấn Anh",
       author_email="ubolonton@gmail.com",
       url="https://github.com/ubolonton/twisted-csp",
       long_description=open('README.md').read(),
       license="EPL",
+
+      packages=find_packages("."),
 
       install_requires=[
           "twisted"

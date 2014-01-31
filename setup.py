94e2adba0db51d43d94601834722a35b7f276703
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-01-31 16:23:17 +0700

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name="twisted-csp",
-      version="0.1.1",
+      version="0.1.2",
       description="Go-style channels for Twisted",
       author="Nguyễn Tuấn Anh",
       author_email="ubolonton@gmail.com",
